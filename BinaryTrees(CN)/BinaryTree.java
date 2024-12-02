9223064be0700d54920b9414ac8380dda2591cf0
Shubham <shubhamrajchauhan07@gmail.com>
2024-12-02 22:59:26 +0530

    PRINT TREE RECURSIVELY 2 --> detailed

--- BinaryTrees(CN)/BinaryTree.java
+++ BinaryTrees(CN)/BinaryTree.java
@@ -1,5 +1,3 @@
-
-
 public class BinaryTree {
     
     public static class BinaryTreeNode<T> {
@@ -28,6 +26,25 @@ public class BinaryTree {
         // }
     }
 
+
+    //PRINT TREE RECURSIVELY 2 --> detailed
+    public static void printTreeDetailed(BinaryTreeNode<Integer> root) {
+        if(root == null) {
+            return;
+        }
+        System.out.print(root.data + ":");
+        if(root.left != null) {
+            System.out.print("L" + root.left.data + ", ");
+        }
+        if(root.right != null) {
+            System.out.print("R" + root.right.data);
+        }
+        System.out.println();
+
+        printTreeDetailed(root.left);
+        printTreeDetailed(root.right);
+    }
+
     public static void main(String[] args) {
         BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(1); //root.data = 1;
 
@@ -36,13 +53,14 @@ public class BinaryTree {
         root.left = rootLeft;
         root.right = rootRight;
 
-        printTree(root);
+        
+        BinaryTreeNode<Integer> twoRight = new BinaryTreeNode<Integer>(4);
+        BinaryTreeNode<Integer> threeLeft = new BinaryTreeNode<Integer>(5);
+        rootLeft.right = twoRight;
+        rootRight.left = threeLeft;
 
-        // BinaryTreeNode<Integer> twoRight = new BinaryTreeNode<Integer>(4);
-        // BinaryTreeNode<Integer> threeLeft = new BinaryTreeNode<Integer>(5);
-
-        // rootLeft.right = twoRight;
-        // rootRight.left = threeLeft;
+        //printTree(root);
+        printTreeDetailed(root);
 
 
         // System.out.println("Tree structure:");
