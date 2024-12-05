f0c9ce1cfa302313b5fdd08c18f92d497a2599b2
Shubham <shubhamrajchauhan07@gmail.com>
2024-12-05 18:01:58 +0530

    1st way

---
+++ RECURSION(KK)/KReverse.java
@@ -0,0 +1,31 @@
+public class KReverse {
+
+    /* 
+    //1st way
+    static int sum = 0;
+    static void rev1(int n) {
+        if(n == 0){
+            return;
+        }
+        int rem = n%10;
+        sum = sum * 10 + rem;
+        rev1(n/10);
+    }
+    */
+
+
+    //2nd way
+    static int rev2(int n) {
+        //sometimes you might need some additional variables in the argument 
+        //in that case, make another functiom
+        
+    }
+    public static void main(String[] args) {
+        /* 
+        rev1(1234);
+        System.out.println(sum);
+        */
+
+
+    }
+}
