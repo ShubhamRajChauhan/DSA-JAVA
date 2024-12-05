86a37deb489468fe4d731af4b440a9b15ecdbe14
Shubham <shubhamrajchauhan07@gmail.com>
2024-12-06 01:13:06 +0530

    find all the index

--- RECURSION(KK)/PFind.java
+++ RECURSION(KK)/PFind.java
@@ -1,9 +1,14 @@
+import java.util.ArrayList;
+
 public class PFind {
     public static void main(String[] args) {
-        int[] arr = {2, 3, 1, 4, 5};
+        int[] arr = {2, 3, 1, 4, 4, 5};
         System.out.println(find(arr, 4, 0));
         System.out.println(findIndex(arr, 4, 0));
         System.out.println(findIndexLast(arr, 4, arr.length-1));
+
+        findAllIndex(arr, 4, 0);
+        System.out.println(list);
     }
     
     //true or false
@@ -37,5 +42,17 @@ public class PFind {
         } else {
             return findIndexLast(arr, target, index-1);
         }    
+    }
+
+    //find all the index
+    static ArrayList<Integer> list = new ArrayList<>();
+    static void findAllIndex(int[] arr, int target, int index) {
+        if(index == arr.length){
+            return;
+        }
+        if(arr[index] == target) {
+            list.add(index);
+        }
+        findAllIndex(arr, target, index+1);
     }
 }
