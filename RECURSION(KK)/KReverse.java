b16e178fb9aca678e4839d03894d42537ae63733
Shubham <shubhamrajchauhan07@gmail.com>
2024-12-05 18:58:10 +0530

    Reverse2

--- RECURSION(KK)/KReverse.java
+++ RECURSION(KK)/KReverse.java
@@ -17,8 +17,16 @@ public class KReverse {
     //2nd way
     static int rev2(int n) {
         //sometimes you might need some additional variables in the argument 
-        //in that case, make another functiom
-        
+        //in that case, make another function
+        int digits = (int)(Math.log10(n)) + 1;
+        return helper(n, digits);     
+    }
+    private static int helper(int n, int digits) {
+        if(n%10 == n) {
+            return n;
+        }
+        int rem = n % 10;
+        return rem * (int)(Math.pow(10, digits-1)) + helper(n/10, digits-1);
     }
     public static void main(String[] args) {
         /* 
@@ -26,6 +34,8 @@ public class KReverse {
         System.out.println(sum);
         */
 
+        System.out.println(rev2(1234));
+        
 
     }
 }
