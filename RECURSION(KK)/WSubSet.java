92b006054bcb1566b22a01291da4ce3cf0696cc6
Shubham <shubhamrajchauhan07@gmail.com>
2024-12-10 20:27:46 +0530

    remove duplicates

--- RECURSION(KK)/WSubSet.java
+++ RECURSION(KK)/WSubSet.java
@@ -1,16 +1,26 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class WSubSet {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3};
-        List<List<Integer>> ans = subset(arr);
+        //1.
+        // int[] arr = {1, 2, 3};
+        // List<List<Integer>> ans = subset(arr);
+        // for(List<Integer> list : ans) {
+        //     System.out.println(list);
+        // }
+
+
+        //2.
+        int[] arr = {1, 2, 2};
+        List<List<Integer>> ans = subsetDuplicate(arr);
         for(List<Integer> list : ans) {
             System.out.println(list);
         }
     }
 
-    //using iteration
+    //1. using iteration
     static List<List<Integer>> subset(int[] arr) {
         List<List<Integer>> outer = new ArrayList<>();
 
@@ -21,6 +31,33 @@ public class WSubSet {
             for(int i = 0; i < n; i++) {
                 List<Integer> internal = new ArrayList<>(outer.get(i));
                 internal.add(num);
+                outer.add(internal);
+            }
+        }
+        return outer;
+    }
+
+
+
+    //2. remove duplicates
+    static List<List<Integer>> subsetDuplicate(int[] arr) {
+        Arrays.sort(arr);
+        List<List<Integer>> outer = new ArrayList<>();
+
+        outer.add(new ArrayList<>());
+        int start = 0;
+        int end = 0;
+        for(int i = 0; i < arr.length; i++) {
+            start = 0;
+            //if current and previous element is same , s = e + 1
+            if(i > 0 && arr[i] == arr[i-1]) {
+                start = end + 1;
+            }
+            end = outer.size() - 1;
+            int n  = outer.size();
+            for(int j = start; j < n; j++) {
+                List<Integer> internal = new ArrayList<>(outer.get(j));
+                internal.add(arr[i]);
                 outer.add(internal);
             }
         }
