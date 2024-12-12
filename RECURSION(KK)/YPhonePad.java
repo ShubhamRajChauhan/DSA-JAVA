8d9bb4da0eed45b55d077ff21654f87562d1ed49
Shubham <shubhamrajchauhan07@gmail.com>
2024-12-12 23:58:16 +0530

    keypad

--- RECURSION(KK)/YPhonePad.java
+++ RECURSION(KK)/YPhonePad.java
@@ -1,6 +1,9 @@
-import java.util.ArrayList;
+import java.util.*;
 
+/* 
 public class YPhonePad {
+
+    
     public static void main(String[] args) {
         //1.
         pad("", "12");
@@ -57,5 +60,30 @@ public class YPhonePad {
     }
 }
 
+*/
+
+
+public class YPhonePad {
+
+    static String[] keypad = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+    static void possibleWords(String s, String ans) {
+        if(s.length() == 0) {
+            System.out.println(ans);
+            return;
+        }
+        String key = keypad[s.charAt(0) - 48];
+
+        for(int i=0; i<key.length(); i++) {
+            possibleWords(s.substring(1), ans + key.charAt(i));
+        }
+    }
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        possibleWords(s, "");
+    }
+}
 
 
