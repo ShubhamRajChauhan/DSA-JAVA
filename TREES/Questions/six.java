1775e983f2e6d8dfa134d0939d6481d025f27d30
Shubham <shubhamrajchauhan07@gmail.com>
2024-12-26 13:05:34 +0530

    101. Symmetric Tree

---
+++ TREES/Questions/six.java
@@ -0,0 +1,37 @@
+//101. Symmetric Tree
+
+import java.util.*;
+
+import javax.swing.tree.TreeNode;
+
+public class six {
+    public boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root.left);
+        queue.add(root.right);
+
+        while(!queue.isEmpty()) {
+            TreeNode left = queue.poll();
+            TreeNode right = queue.poll();
+
+            if(left == null && right == null) {
+                continue;
+            }
+
+            if(left == null || right == null) {
+                return false;
+            }
+
+            if(left.val != right.val) {
+                return false;
+            }
+
+            queue.add(left.left);
+            queue.add(right.right);
+            queue.add(left.right);
+            queue.add(right.left);
+
+        }
+        return true;
+    }
+}
