6e62d134312462ad28d3bf23b670ff5554b960dd
Shubham <shubhamrajchauhan07@gmail.com>
2024-12-28 18:00:51 +0530

    112. Path Sum

---
+++ TREES/Questions/fifteen.java
@@ -0,0 +1,17 @@
+//112. Path Sum
+
+import javax.swing.tree.TreeNode;
+
+public class fifteen {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root == null) {
+            return false;
+        }
+
+        if (root.val == targetSum && root.left == null && root.right == null) {
+            return true;
+        }
+
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
+}
