f39b6e6629cf0c0ce93f2c86c1b5104835565aa3
Shubham <shubhamrajchauhan07@gmail.com>
2025-01-14 16:10:36 +0530

    Majority Element

--- HASHING/sixMajorityElement.java
+++ HASHING/sixMajorityElement.java
@@ -3,7 +3,8 @@ import java.util.*;
 
 public class sixMajorityElement {
     public static void main(String[] args) {
-        int arr[] = { 1, 3, 2, 5, 1, 3, 1, 5, 1};
+        // int arr[] = { 1, 3, 2, 5, 1, 3, 1, 5, 1};
+        int arr[] = {1,2};
         HashMap<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < arr.length; i++) {
@@ -26,6 +27,7 @@ public class sixMajorityElement {
         for(Integer key : map.keySet()) {
             if(map.get(key) > arr.length/3) {
                 System.out.println(key);
+                //break;  -> to print only one value;
             }
         }
     }
