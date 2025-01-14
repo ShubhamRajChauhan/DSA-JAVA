b383859b51ce1747477157fbe4499e9244d6d326
Shubham <shubhamrajchauhan07@gmail.com>
2025-01-14 23:09:44 +0530

    Iteration on HashSets

---
+++ HASHING/nineIterationOnHashSets.java
@@ -0,0 +1,23 @@
+//-------Iteration on HashSets-------------------
+import java.util.*;
+
+public class nineIterationOnHashSets {
+    public static void main(String[] args) {
+        HashSet<String> cities = new HashSet<>();
+        cities.add("Delhi");
+        cities.add("Mumbai");
+        cities.add("Noida");
+        cities.add("Bengaluru");
+
+        /*
+        Iterator it = cities.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+        */
+
+        for(String city : cities) {
+            System.out.println(city);
+        }
+    }
+}
