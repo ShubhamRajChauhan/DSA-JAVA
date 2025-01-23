2174e915323a31ad1b6003313d99521c79dc439a
Shubham <shubhamrajchauhan07@gmail.com>
2025-01-24 02:16:51 +0530

    tries

--- TRIES/ATries.java
+++ TRIES/ATries.java
@@ -4,7 +4,7 @@ public class ATries {
         Node children[] = new Node[26];
         boolean eow = false;
 
-        Node() {
+        public Node() {
             for(int i=0; i<26; i++) {
                 children[i] = null;
             }
