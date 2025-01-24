25be259dfc2b8334bd03f1f6817c9e53e96b8592
Shubham <shubhamrajchauhan07@gmail.com>
2025-01-25 02:12:23 +0530

    COUNT UNIQUE SUBSTRINGS

---
+++ TRIES/DCountUniqueSubstrings.java
@@ -0,0 +1,71 @@
+//COUNT UNIQUE SUBSTRINGS 
+
+public class DCountUniqueSubstrings {
+    static class Node {
+        Node children[] = new Node[26];
+        boolean eow = false;
+
+        public Node() {
+            for(int i=0; i<26; i++) {
+                children[i] = null;
+            }
+        }
+    }
+
+    public static Node root = new Node();
+
+    //insert 0(L)   (L = length of largest word)
+    public static void insert(String word) {
+        Node curr = root;
+        for(int level=0; level<word.length(); level++) {
+            int idx = word.charAt(level) - 'a';
+            if(curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+    }
+
+
+    //search 0(L)   (L = length of largest word)
+    public static boolean search(String key) {
+        Node curr = root;
+        for(int level=0; level<key.length(); level++) {
+            int idx = key.charAt(level) - 'a';
+            if(curr.children[idx] == null) {
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return curr.eow == true;
+    }
+
+    //STEP 3 => count nodes
+    public static int countNodes(Node root) {
+        if(root == null) {
+            return 0;
+        }
+        
+        int count = 0;
+        for(int i=0; i<26; i++) {
+            if(root.children[i] != null) {
+                count += countNodes(root.children[i]);
+            }
+        }
+        return count + 1;
+    }
+    
+    public static void main(String[] args) {
+        String str = "ababa";
+
+        //STEP 1,2 => find suffix and insert in trie
+        for(int i=0; i<str.length(); i++){
+            String suffix = str.substring(i);
+            insert(suffix);
+        }
+
+        System.out.println(countNodes(root));  //10
+      
+    }
+}
