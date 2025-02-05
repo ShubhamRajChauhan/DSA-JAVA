9f867c9b7f1a4f2ddbe23e0276fcbce8e1506438
Shubham <shubhamrajchauhan07@gmail.com>
2025-02-05 18:22:50 +0530

    653 - leetcode

---
+++ TREES/Advance/four.java
@@ -0,0 +1,22 @@
+//653. Two Sum IV - Input is a BST
+//best question to understand recursion 
+import java.util.*;
+
+public class four {
+    public boolean findTarget(TreeNode root, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        return helper(root, k, set);
+    }
+
+    private boolean helper(TreeNode node, int k, HashSet<Integer> set) {
+        if (node == null) {
+            return false;
+        }
+        if (set.contains(k - node.val)) {
+            return true;
+        }
+
+        set.add(node.val);
+        return helper(node.left, k, set) || helper(node.right, k, set);
+    }
+}
