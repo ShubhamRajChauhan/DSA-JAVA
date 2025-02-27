dc1a6134129740da919beab555a963f57f374099
Shubham <shubhamrajchauhan07@gmail.com>
2025-02-28 00:53:11 +0530

    Cycle Detection (Undirected Graph) using DFS

--- GRAPH/seven.java
+++ GRAPH/seven.java
@@ -36,12 +36,62 @@ public class seven {
     }
 
 
+
+
+    //cycle detection   TC: 0(V+E)
+    public static boolean detectCycle(ArrayList<Edge>[] graph) {
+        boolean vis[] = new boolean[graph.length];
+        for(int i=0; i<graph.length; i++) {
+            if(!vis[i]) {
+                if(detectCycleUtil(graph, vis, i, -1)) {
+                    return true;
+                    //cycle exists in one of the parts
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean detectCycleUtil(ArrayList<Edge>[] graph, boolean vis[], int curr, int par) {
+        vis[curr] = true;
+
+        for(int i=0; i<graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+            //case 3:
+            if(!vis[e.dest]) {
+                if(detectCycleUtil(graph, vis, e.dest, curr)){
+                    return true;
+                }
+            }
+            //case 1:
+            else if(vis[e.dest] && e.dest != par) {
+                return true;
+            }
+            //case 2: do nothing -> continue
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
-        int V = 7;
+        /*
+         
+             0----------3
+            / |         |
+           /  |         |
+          1   |         |
+           \  |         |
+            \ |         |
+             2          4
+
+        */
+
+
+        int V = 5;
         ArrayList<Edge>[] graph = new ArrayList[V]; 
         createGraph(graph);
-        
+        System.out.println(detectCycle(graph));
     }
 }
 
 
+//true
