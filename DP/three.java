88850e6e90c77dd8cda3b6f190bb30b3164789a1
Shubham <shubhamrajchauhan07@gmail.com>
2025-04-07 23:03:08 +0530

    USING ITERATIVE DP

--- DP/three.java
+++ DP/three.java
@@ -28,7 +28,9 @@ public class three {
 */
 
 
+
 //-------------------USING RECURSIVE DP--------------------------
+/* 
 public class three {
 
     public static int minSquares(int n, int[] dp) {
@@ -59,6 +61,36 @@ public class three {
         }
 
         int ans = minSquares(n, dp);
+        System.out.println(ans);
+    }
+}
+*/
+
+
+
+//--------------USING ITERATIVE DP------------------
+public class three {
+
+    public static int minSquares(int n) {
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+
+        for(int i=1; i<=n; i++) {
+
+            int minAns = Integer.MAX_VALUE;
+            for(int j=1; j*j<=i; j++) {
+                int currAns = dp[i-(j*j)];
+                if(minAns > currAns) {
+                    minAns = currAns;
+                }
+            }
+            dp[i] = 1 + minAns;
+        }
+        return dp[n];
+    }
+    public static void main(String[] args) {
+        int n = 7;
+        int ans = minSquares(n);
         System.out.println(ans);
     }
 }
