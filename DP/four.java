ecbc51c567ee30f877f1caae3026a5fddd3c4302
Shubham <shubhamrajchauhan07@gmail.com>
2025-04-11 00:46:16 +0530

    Min Cost Path

--- DP/four.java
+++ DP/four.java
@@ -34,7 +34,9 @@ public class four {
 
 
 
+
 //----------------------Memoization---------------------------------
+/* 
 public class four {
 
     public static int minCost(int[][] cost, int i, int j, int[][] dp) {
@@ -80,7 +82,7 @@ public class four {
         int[][] cost = {{1,5,11}, {8,13,12}, {2,3,7}, {15,16,18}};
         int[][] dp = new int[cost.length+1][cost[0].length+1];
         for(int i=0; i<dp.length; i++) {
-            for(int j=0; j<dp[0].length; j++) {
+            for(int j=0; j<dp[i].length; j++) {
                 dp[i][j] = Integer.MIN_VALUE;
             }
         }
@@ -88,3 +90,46 @@ public class four {
         System.out.println(ans);
     }
 }
+*/
+
+
+
+
+//----------------------USING ITERATIVE DP---------------------------------
+public class four {
+
+    public static int minCost(int[][] cost) {
+        int m = cost.length;
+        int n = cost[0].length;
+
+        int[][] dp = new int[m+1][n+1];
+        for(int i=0; i<dp.length; i++) {
+            for(int j=0; j<dp[i].length; j++) {
+                dp[i][j] = Integer.MAX_VALUE;
+            }
+        }
+
+        for(int i=m-1; i>=0; i--) {
+            for(int j=n-1; j>=0; j--) {
+                if(i==m-1 && j==n-1) {
+                    dp[i][j] = cost[i][j];
+                    continue;
+                }
+                int ans1 = dp[i][j+1];
+                int ans2 = dp[i+1][j];
+                int ans3 = dp[i+1][j+1];
+                dp[i][j] = cost[i][j] + Math.min(ans1, Math.min(ans2, ans3));
+            }
+        }
+        return dp[0][0];
+    }
+
+    public static void main(String[] args) {
+        int[][] cost = {{1,5,11}, {8,13,12}, {2,3,7}, {15,16,18}};
+        int ans = minCost(cost);
+        System.out.println(ans);
+    }
+}
+
+
+//Output : 30
