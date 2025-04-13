cc3ca4602ab01d68c74737de78e5b6543da277ac
Shubham <shubhamrajchauhan07@gmail.com>
2025-04-13 10:53:33 +0530

    0-1 Knapsack - iterative

--- DP/six.java
+++ DP/six.java
@@ -1,6 +1,7 @@
 // 0-1 Knapsack
 
 //-----------------Recursion-------------------------------
+/* 
 public class six {
 
     public static int knapsack(int W, int val[], int wt[], int i) {
@@ -27,6 +28,45 @@ public class six {
         int W = 50;
 
         int ans = knapsack(W, val, wt, 0);
+        System.out.println(ans);
+    }
+}
+*/
+
+
+
+
+
+//-----------------Iterative DP-------------------------------
+public class six {
+
+    public static int knapsack(int W, int val[], int wt[]) {
+        int n = val.length;
+        int[][] dp = new int[n+1][W+1];
+
+        for(int i=n-1; i>=0; i--) {
+            for(int w=0; w<=W; w++) {
+
+                int ans;
+                if(wt[i] <= w) {
+                    int ans1 = val[i] + dp[i+1][w-wt[i]];
+                    int ans2 = dp[i+1][w];
+                    ans = Math.max(ans1, ans2);
+                }else {
+                    ans = dp[i+1][w];
+                }
+                dp[i][w] = ans;
+            }
+        }
+        return dp[0][W];
+        
+    }
+    public static void main(String[] args) {
+        int val[] = {200, 300, 100};
+        int wt[] = {20, 25, 30};
+        int W = 50;
+
+        int ans = knapsack(W, val, wt);
         System.out.println(ans);
     }
 }
