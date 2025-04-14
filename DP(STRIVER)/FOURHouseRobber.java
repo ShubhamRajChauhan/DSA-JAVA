a44073b58b04b452ebde112117b265b8d4e6fab0
Shubham <shubhamrajchauhan07@gmail.com>
2025-04-14 23:15:28 +0530

    House Robber  --> Leetcode = 198

--- DP(STRIVER)/FOURHouseRobber.java
+++ DP(STRIVER)/FOURHouseRobber.java
@@ -50,3 +50,27 @@ class Solution {
         return dp[n-1];
     }
 }
+
+
+//-------------------Space Optimization---------------------------
+//TC: 0(n) , SC:0(1)
+class Solution1 {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int prev = nums[0];
+        int prev2 = 0;
+        for(int i=1; i<n; i++) {
+            int pick = nums[i];
+            if(i > 1) {
+                pick += prev2;
+            }
+
+            int nonpick = 0 + prev;
+
+            int curi = Math.max(pick, nonpick);
+            prev2 = prev;
+            prev = curi;
+        }
+        return prev;
+    }
+}
