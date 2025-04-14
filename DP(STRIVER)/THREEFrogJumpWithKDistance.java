937d6c4bd3a0a5f81f62ecf459c71cbc301e108e
Shubham <shubhamrajchauhan07@gmail.com>
2025-04-14 12:33:31 +0530

    Frog Jump With K Distance -> Striver

---
+++ DP(STRIVER)/THREEFrogJumpWithKDistance.java
@@ -0,0 +1,23 @@
+//Frog Jump With K Distance -> Striver
+
+public class THREEFrogJumpWithKDistance {
+    public int frogJump(int[] heights, int k) {
+        int n = heights.length;
+        int[] dp = new int[n];
+        dp[0] = 0;
+
+        for (int i = 1; i < n; i++) {
+            int minSteps = Integer.MAX_VALUE;
+            for (int j = 1; j <= k; j++) {
+                if (i - j >= 0) {
+                    int jump = dp[i - j] + Math.abs(heights[i] - heights[i - j]);
+                    minSteps = Math.min(minSteps, jump);
+                }
+            }
+            dp[i] = minSteps; 
+        }
+
+        return dp[n - 1];
+    }
+}
+
