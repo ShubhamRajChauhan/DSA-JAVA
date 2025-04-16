b0c7c8108c2a9bbb599659b7ab3073038167ca05
Shubham <shubhamrajchauhan07@gmail.com>
2025-04-16 21:08:04 +0530

    Ninja's Training -> Striver

--- DP(STRIVER)/SIXNinjaTraining.java
+++ DP(STRIVER)/SIXNinjaTraining.java
@@ -88,8 +88,10 @@ public class SIXNinjaTraining {
 
 
 
-//------------------------Tabulation-------------------------------------
 
+
+//------------------------Tabulation-------------------------------------
+/* 
 public class SIXNinjaTraining {
     
     public static int ninjaTraining(int n, int[][] points) {
@@ -112,5 +114,40 @@ public class SIXNinjaTraining {
             }
         }
         return dp[n-1][3];
+    }
+}
+*/
+
+
+
+
+
+
+
+//------------------------Space Optimization-------------------------------------
+
+public class SIXNinjaTraining {
+    
+    public static int ninjaTraining(int n, int[][] points) {
+        int[] prev = new int[4];
+
+        prev[0] = Math.max(points[0][1], points[0][2]);
+        prev[1] = Math.max(points[0][0], points[0][2]);
+        prev[2] = Math.max(points[0][0], points[0][1]);
+        prev[3] = Math.max(points[0][0], Math.max(points[0][1], points[0][2]));
+
+        for(int day=1; day<n; day++) {
+            int[] temp = new int[4];
+            for(int last=0; last<4; last++) {
+                temp[last] = 0;
+                for(int task=0; task<3; task++) {
+                    if(task != last) {
+                        temp[last] = Math.max(temp[last], points[day][task] + prev[task]);
+                    }
+                }
+            }
+            prev = temp;
+        }
+        return prev[3];
     }
 }
